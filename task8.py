1d8184b28c1578a815f8b11cde128163b6f4f665
leaty-ell <lisalimanova14@gmail.com>
2025-03-02 20:31:12 +0700

    third commit

--- task8.py
+++ task8.py
@@ -5,7 +5,7 @@ b = float(input("Введите сторону b: "))
 c = float(input("Введите сторону c: "))
 
 if a + b > c and a + c > b and b + c > a:
-    # используем т. косинусов: a**2 = b**2 + c**2 - 2*b*c * acos(A)
+    # используем т. косинусов: a**2 = b**2 + c**2 - 2*b*c * cos(A)
     alpha = math.degrees(math.acos((b**2 + c**2 - a**2) / (2 * b * c)))
     beta = math.degrees(math.acos((a**2 + c**2 - b**2) / (2 * a * c)))
     gamma = math.degrees(math.acos((a**2 + b**2 - c**2) / (2 * a * b)))
